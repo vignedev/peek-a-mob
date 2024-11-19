8a4d57f3ef873987b46274429606dd2d683a8504
vignedev <vignedev@gmail.com>
2024-11-19 19:29:21 +0100

    feat: add env config for proxy and hosting to frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: process.env['HOST'] || '127.0.0.1',
+    port: parseInt(process.env['PORT']!, 10) || 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000'
+        target: process.env['PROXY_API'] || 'http://127.0.0.1:8000'
       }
     }
   }
