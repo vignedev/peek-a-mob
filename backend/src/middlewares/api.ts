6bc1759ca1ff737e6570ca758f0651c79081e104
vignedev <vignedev@gmail.com>
2024-11-19 19:45:43 +0100

    feat: more verbose error responses

--- backend/src/middlewares/api.ts
+++ backend/src/middlewares/api.ts
@@ -1,34 +1,50 @@
 import restana, { Protocol } from 'restana'
-import { detections, videos, entities } from '../libs/database'
+import * as database from '../libs/database'
 
 const detectionsApi = (router: restana.Router<Protocol.HTTP>) => {
   return router
-    .get('/entities', async (req, res) => {
-      return res.send(await entities.getAll(), 200)
+    .get('/entities', async (_req, res) => {
+      return res.send(await database.entities.getAll(), 200)
     })
-    .get('/videos', async (req, res) => {
-      return res.send(await videos.getAll(), 200)
+    .get('/videos', async (_req, res) => {
+      return res.send(await database.videos.getAll(), 200)
     })
     .get('/videos/:id', async (req, res) => {
       const { id: videoId } = req.params
 
-      if (!videoId) throw new Error('Missing video (?v=) query string!')
-      if (Array.isArray(videoId)) throw new Error('Multiple videos (?v=) in query string!')
+      const video = await database.videos.get(videoId)
+      if (!video)
+        return res.send({ error: 'No video of such ID was found.' }, 404)
 
-      return res.send(await videos.get(videoId), 200)
+      return res.send(video)
     })
     .get('/videos/:videoId/detections/:modelName', async (req, res) => {
       const { videoId, modelName } = req.params
-      const { entities, ss: timeStart, to: timeEnd, conf: confidence } = req.query
+      const { entities, ss, to, conf } = req.query
 
-      if (Array.isArray(timeStart) || Array.isArray(timeEnd)) throw new Error('Invalid time range!')
+      if (Array.isArray(ss) || Array.isArray(to))
+        return res.send({ error: 'Invalid time range (multiple ranges?)' }, 400)
+
+      const [timeStart, timeEnd] = [ss || '0', to || 'Infinity'].map(x => parseFloat(x))
+      if (isNaN(timeStart) || isNaN(timeEnd))
+        return res.send({ error: '?ss= or ?to= value could not be parsed!' }, 400)
+
+      if (timeStart > timeEnd)
+        return res.send({ error: 'Invalid time range (start > end)' }, 400)
+
+      if (Array.isArray(conf))
+        return res.send({ error: 'Confidence cannot be an array!' }, 400)
+
+      const confidence = parseFloat(conf || '0.65')
+      if (isNaN(confidence))
+        return res.send({ error: 'Confidence value could not be parsed' }, 400)
 
       return res.send(
-        await detections.get(videoId, decodeURIComponent(modelName), {
+        await database.detections.get(videoId, decodeURIComponent(modelName), {
           entityNames: entities ? (Array.isArray(entities) ? entities : [entities]) : [],
-          confidence: parseFloat(Array.isArray(confidence) ? confidence[0] : confidence) || 0.65,
-          timeStart: timeStart ? parseFloat(timeStart) : 0,
-          timeEnd: timeEnd ? parseFloat(timeEnd) : Infinity
+          confidence: confidence,
+          timeStart: timeStart,
+          timeEnd: timeEnd
         }),
         200
       )
