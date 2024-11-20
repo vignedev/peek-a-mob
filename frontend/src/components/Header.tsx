a617c9f9068efda5dff1df86d368ebc1dc870730
Z9Master <hoangdinhtu72@gmail.com>
2024-11-20 02:26:51 +0100

    feat: add header, styling, request link

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,28 @@
-import { Flex } from "@radix-ui/themes";
+import { Button, Flex } from "@radix-ui/themes";
 import { PeekAMobHeading } from "./Branding";
+import { MagnifyingGlassIcon, FilePlusIcon } from "@radix-ui/react-icons"
 import Search from "./Search";
+import { useNavigate } from "react-router-dom";
 
 const SearchHeader = () => {
-    return (
-        <Flex gap="2" width="auto">
-            <PeekAMobHeading />
-            <Search />
-        </Flex>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <Flex gap="8" width="auto">
+      <PeekAMobHeading />
+      <Flex gap="2" width="100%">
+        <Search />
+        <Button variant="outline" highContrast style={{height: "auto", cursor: "pointer"}}>
+          <MagnifyingGlassIcon /> Search
+        </Button>
+      
+      <Button variant="outline" style={{height: "auto", cursor: "pointer"}} 
+        onClick={() => { navigate("/request") }}>
+          <FilePlusIcon /> Request
+      </Button>
+      </Flex>
+    </Flex>
+  )
 }
 
 export default SearchHeader;
