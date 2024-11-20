b8bd55e6897a3fa1064530717ad538d6fd82615a
vignedev <vignedev@gmail.com>
2024-11-20 05:12:02 +0100

    chore: limit resoluition to 1080p and h264, no hls, flush often

--- scripts/analyze-youtube.py
+++ scripts/analyze-youtube.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
   video = None
 
   from yt_dlp import YoutubeDL
-  with YoutubeDL({ 'quiet': 'please' }) as ytdl:
+  with YoutubeDL({ 'quiet': 'please', 'format': 'bestvideo[height<=1080][ext=mp4][vcodec^=avc]' }) as ytdl:
     info = ytdl.extract_info(argv.url, download=False)
     if info is None:
       print(f'> YouTube didn\'t return a video result, sad tako...', file=sys.stderr)
@@ -46,7 +46,12 @@ if __name__ == '__main__':
 
     # TODO: naive and stupid way -- yt_dlp usually selects bestvideo+bestaudio (usually highest)
     #       so we take the *first id*, which should be the video ID (could easily fail though)
-    fmt = info['requested_formats'][0]
+    fmt = {
+      'format': info['format'],
+      'format_note': info['format'],
+      'format_id': info['format_id'],
+      'url': info['url']
+    }
 
     print(f'> Analyzing "{video['title']}" ({video['id']}, {video['width']}x{video['height']} @ {video['fps']}) from "{video['channel']}", long {video['duration']} seconds', file=sys.stderr)
     print(f'> Using format_id={fmt['format_id']} aka {fmt['format_note']}', file=sys.stderr)
@@ -93,6 +98,9 @@ if __name__ == '__main__':
     while cap.isOpened():
       ret, frame = cap.read()
 
+      if ret is False:
+        continue
+
       for result in yoloModel.predict(
         source=frame,
         conf=argv.conf,
@@ -103,7 +111,7 @@ if __name__ == '__main__':
         show=argv.show,
       ):
         if result.boxes is None:
-          print(f'> ??? result.boxes is None?', file=sys.stderr)
+          sys.stderr.write(f'> ??? result.boxes is None?\n', file=sys.stderr)
           continue
 
         for box in result.boxes:
@@ -128,7 +136,8 @@ if __name__ == '__main__':
               'average': avg_rate,
               'last': time_diff
             }
-          }))
+          }) + '\n')
+          sys.stdout.flush()
         else:
           sys.stderr.write(f'\r{(frame_pos/vFrames*100.0):.2f}% | {datetime.timedelta(seconds=math.floor(frame_pos / vFPS))} - {vFramesStr} | {frame_pos}/{vFrames} | {found_total} | {(1.0 / time_diff):.2f} FPS ({time_diff:.1f} ms) | {(1.0 / avg_rate):.2f} aFPS ({avg_rate:.1f} ms) | ETA: {datetime.timedelta(seconds=math.floor((vFrames - frame_pos) * avg_rate))}         ')
 
@@ -137,6 +146,19 @@ if __name__ == '__main__':
 
       frame_pos += 1
       if frame_pos == vFrames:
+        if argv.json:
+          sys.stdout.write(json.dumps({
+            'currentFrame': frame_pos,
+            'totalFrames': vFrames,
+            'rate': {
+              'average': avg_rate,
+              'last': time_diff
+            }
+          }) + '\n')
+          sys.stdout.flush()
+        else:
+          sys.stderr.write(f'\r{(frame_pos/vFrames*100.0):.2f}% | {datetime.timedelta(seconds=math.floor(frame_pos / vFPS))} - {vFramesStr} | {frame_pos}/{vFrames} | {found_total} | {(1.0 / time_diff):.2f} FPS ({time_diff:.1f} ms) | {(1.0 / avg_rate):.2f} aFPS ({avg_rate:.1f} ms) | ETA: {datetime.timedelta(seconds=math.floor((vFrames - frame_pos) * avg_rate))}         ')
+
         cap.release()
         break
 
