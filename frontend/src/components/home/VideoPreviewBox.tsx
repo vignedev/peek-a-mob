4bd19a87f1da4bb2e4f9d9f5ffc6e693235c22be
Dinh Tu Hoang <hoangdin@fit.cvut.cz>
2024-11-20 09:49:25 +0100

    feat: add demo video thumbnail preview

---
+++ frontend/src/components/home/VideoPreviewBox.tsx
@@ -0,0 +1,61 @@
+import { Flex, Text } from "@radix-ui/themes"
+import { useNavigate } from "react-router-dom"
+
+const VideoPreviewBox = () => {
+  const navigate = useNavigate();
+  return (
+    <Flex 
+      direction="column" 
+      onClick={() => navigate("/debug")
+    }>
+      <img 
+        src="https://img.youtube.com/vi/uEvwuvod2F4/mqdefault.jpg" 
+        width="100%"
+        style={{
+          borderRadius:"max(var(--radius-2), var(--radius-full))"
+        }}
+      />
+      <Text>【Minecraft】 Together As One!!! #MythOneblock</Text>
+      <Flex gap="2" wrap="wrap">
+        <Text style={{
+          borderStyle:"solid",
+          borderColor:"grey",
+          borderWidth: "2px",
+          borderRadius:"max(var(--radius-2), var(--radius-full))",
+          padding: "0px 8px"
+        }}>
+          Enderman
+        </Text>
+        <Text style={{
+          borderStyle:"solid",
+          borderColor:"grey",
+          borderWidth: "2px",
+          borderRadius:"max(var(--radius-2), var(--radius-full))",
+          padding: "0px 8px"
+        }}>
+          Chicken
+        </Text>
+        <Text style={{
+          borderStyle:"solid",
+          borderColor:"grey",
+          borderWidth: "2px",
+          borderRadius:"max(var(--radius-2), var(--radius-full))",
+          padding: "0px 8px"
+        }}>
+          Zombie
+        </Text>
+        <Text style={{
+          borderStyle:"solid",
+          borderColor:"grey",
+          borderWidth: "2px",
+          borderRadius:"max(var(--radius-2), var(--radius-full))",
+          padding: "0px 8px"
+        }}>
+          Pig
+        </Text>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default VideoPreviewBox
