5e96dc5341c2ae7a10a431edfd2755cb00dd70c3
vignedev <vignedev@gmail.com>
2024-11-27 11:48:35 +0100

    fix: dotenv not loading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: process.env['HOST'] || '127.0.0.1',
-    port: parseInt(process.env['PORT']!, 10) || 3000,
-    proxy: {
-      '/api': {
-        target: process.env['PROXY_API'] || 'http://127.0.0.1:8000'
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    server: {
+      host: env['HOST'] || '127.0.0.1',
+      port: parseInt(env['PORT']!, 10) || 3000,
+      proxy: {
+        '/api': {
+          target: env['PROXY_API'] || 'http://127.0.0.1:8000'
+        }
       }
     }
   }
