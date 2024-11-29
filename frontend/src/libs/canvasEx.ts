2f1e4c4a98c077dfa2ffe2a3ff76fc03e3938a0e
vignedev <vignedev@gmail.com>
2024-11-29 01:39:03 +0100

    feat: pixel-perfect line drawing

--- frontend/src/libs/canvasEx.ts
+++ frontend/src/libs/canvasEx.ts
@@ -1,13 +1,15 @@
 export type CanvasRenderingContext2DExpanded = CanvasRenderingContext2D & {
-  drawLine(x1: number, y1: number, x2: number, y2: number, lineWidth?: number, strokeStyle?: string): void,
+  drawLine(x1: number, y1: number, x2: number, y2: number, lineWidth?: number, strokeStyle?: string, integer?: boolean): void,
   drawBoundingBox(x1: number, y1: number, x2: number, y2: number, text: string, color: string): void,
 }
 
 export function expandContext(ctx: CanvasRenderingContext2D): CanvasRenderingContext2DExpanded {
   const prepare = ctx as CanvasRenderingContext2DExpanded
-  prepare.drawLine = (x1, y1, x2, y2, lineWidth, strokeStyle) => {
+  prepare.drawLine = (_x1, _y1, _x2, _y2, lineWidth, strokeStyle, integer = false) => {
+    const [x1, y1, x2, y2] = [_x1, _y1, _x2, _y2].map(x => integer ? Math.round(x) : x)
+
     if (lineWidth != null || typeof lineWidth !== 'undefined')
-      ctx.lineWidth = lineWidth
+      ctx.lineWidth = integer ? Math.ceil(lineWidth) : lineWidth
     if (strokeStyle != null || typeof strokeStyle !== 'undefined')
       ctx.strokeStyle = strokeStyle
     ctx.beginPath()
