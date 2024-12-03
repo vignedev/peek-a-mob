87e6af37a68f0c4832f47ed882ab0464e18fdfa5
Dinh Tu Hoang <hoangdin@fit.cvut.cz>
2024-12-03 17:43:36 +0100

    chore: add gap homepage (vigtheorem and videos)

--- frontend/src/routes/home.tsx
+++ frontend/src/routes/home.tsx
@@ -6,9 +6,12 @@ import HomeVideoGrid from "../components/home/HomeVideoGrid"
 const HomePage = () => {
   const maxHomePageVideos = 9;
   return(
-    <>
+    <Flex
+      direction='column'
+      gap='4'  
+    >
       <Flex
-        height='calc(100vh - 10rem)'
+        height='calc(100vh - 11.5rem)'
         align='center'
         justify='center'
         gap="4"
@@ -17,7 +20,7 @@ const HomePage = () => {
         <HomeVideoGrid maxHomePageVideos={maxHomePageVideos} />
       </Flex>
       <VigTheorem />
-    </>
+    </Flex>
   )
 }
 
