ae87803461bd2626558eb1d11d499dd6b4a37d86
vignedev <vignedev@gmail.com>
2024-12-04 15:45:08 +0100

    dirty_chore: idk man headache

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,16 +1,34 @@
 import Select, { MultiValue } from 'react-select'
 import { grayDark } from "@radix-ui/colors";
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const Search = (props: {
   options: { value: string, label: string }[],
   setSelectedEntities: (entities: string[]) => void
 }) => {
+  const location = useLocation()
+
+  const [localValue, setLocalValue] = useState<{ value: string, label: string }[]>([])
+
   const handleEntityInputChange = (selected: MultiValue<{
     value: string;
     label: string;
   }>) => {
+    setLocalValue(selected as unknown as any)
     props.setSelectedEntities(selected.map((value) => value.value))
   }
+
+  useEffect(() => {
+    if (location.pathname == '/') {
+      setLocalValue([])
+      return
+    } else if (location.pathname != '/search-detail')
+      return;
+
+    setLocalValue((location.state?.currentEntities || []).map((x: string) => ({ value: x, label: x })))
+  }, [location.pathname])
+
 
   return (
     <div style={{ width: "100%" }}>
@@ -18,6 +36,7 @@ const Search = (props: {
         options={props.options}
         className="basic-multi-select"
         isMulti
+        value={localValue}
         onChange={handleEntityInputChange}
         styles={{
           control: (baseStyles) => ({
@@ -34,7 +53,8 @@ const Search = (props: {
             ...baseStyles,
             backgroundColor: grayDark.gray4,
             borderRadius: "max(var(--radius-2), var(--radius-full))",
-            boxShadow: "var(--shadow-3)"
+            boxShadow: "var(--shadow-3)",
+            zIndex: 3
           }),
           option: (baseStyles, state) => ({
             ...baseStyles,
--- frontend/src/routes/search-detail.tsx
+++ frontend/src/routes/search-detail.tsx
@@ -1,6 +1,5 @@
 import { Flex } from "@radix-ui/themes";
 import SearchDetailVideoSearch from "../components/search-detail/SearchDetailVideoSearch";
-import SearchRetailConfigBar from "../components/search-detail/SearchDetailConfigBar";
 import SearchDetailVideoBar from "../components/search-detail/SearchDetailVideoBar";
 import { useLocation } from "react-router-dom";
 import { Video } from "../libs/api";
@@ -17,29 +16,29 @@ const SearchDetailPage = () => {
 
   return (
     <>
-    {
-      currentVideo &&
-      <Flex
-        height='calc(100vh - 10rem)'
-        align='center'
-        justify='center'
-        gap="4"
-      >
-        <Flex direction="column" gap="4" height="100%" width="40rem">
-          <SearchDetailVideoBar
-            currentVideoId={state.currentVideoId}
-            videoList={state.videoList}
-            modelId={state.modelId}
+      {
+        currentVideo &&
+        <Flex
+          height='calc(100vh - 10rem)'
+          align='center'
+          justify='center'
+          gap="4"
+        >
+          <Flex direction="column" gap="4" height="100%" width="40rem">
+            <SearchDetailVideoBar
+              currentVideoId={state.currentVideoId}
+              videoList={state.videoList}
+              modelId={state.modelId}
+              entities={state.currentEntities}
+            />
+          </Flex>
+          <SearchDetailVideoSearch
+            video={currentVideo}
             entities={state.currentEntities}
+            modelId={state.modelId}
           />
         </Flex>
-        <SearchDetailVideoSearch
-          video={currentVideo}
-          entities={state.currentEntities}
-          modelId={state.modelId}
-        />
-      </Flex>
-    }
+      }
     </>
   )
 }
